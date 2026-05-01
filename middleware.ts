f78ea9e0a4846da949067f148fd225535ed6e6d3
fozilov-16 <fazylovmuhammad1606@gmail.com>
2026-05-01 17:56:11 +0500

    All Done

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './src/i18n/routing';
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./src/i18n/routing";
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  localePrefix: "always"
+});
 
 export const config = {
   matcher: ["/((?!api|trpc|_next|.*\\..*).*)"]
